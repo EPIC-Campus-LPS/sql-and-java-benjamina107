fea2565984f15f5761f3626ffd9759e457ac7348
benjamina107 <benjamina107@lpsk12.org>
2024-03-19 11:03:30 -0600

    functining

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,41 @@ public class Main {
 
         SQL sql = new SQL();
 
-        Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println();
-        System.out.println("Input a number");
-        System.out.println();
-
-        String word = scanner.next();
-
-
-
-
-
-
-
-
-
-
-
         int num = (int) (889 * Math.random() + 1);
 
         System.out.print(sql.getFromNumber("pokedex", num, "name"));
         System.out.print(", the ");
         System.out.println(sql.getFromNumber("pokedex", num, "species"));
 
+        System.out.println("corresponds to the song");
+
         System.out.print(sql.getFromNumber("spotify", num, "name"));
         System.out.print(", by ");
         System.out.println(sql.getFromNumber("spotify", num, "Artist"));
+
+        System.out.println("and the company ");
 
         System.out.print(sql.getFromNumber("topcompanies", num, "name"));
         System.out.print(", from ");
         System.out.println(sql.getFromNumber("topcompanies", num, "Country"));
 
+        System.out.println();
+
+        sql.addEntry(99999, "doody");
+
+        System.out.println("added item");
+
+        System.out.print(sql.getFromNumber("topcompanies", 99999, "name"));
+
+        sql.removeEntry(99999);
+
+        System.out.print(", removed item");
+
+        sql.addEntry(99999, "doody");
+        sql.updateEntry(99999, "Skibodi touelet");
+
+        System.out.print(", updated to ");
+
+        System.out.print(sql.getFromNumber("topcompanies", 99999, "name"));
     }
 }
